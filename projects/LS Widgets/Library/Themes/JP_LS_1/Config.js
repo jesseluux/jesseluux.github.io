a533ffc55f564d08ff34f6881af4b609c692b7f8
jesseluux <jesse96pesola@gmail.com>
2016-08-30 19:57:26 +0000

    Added theme

---
+++ projects/LS Widgets/Library/Themes/JP_LS_1/Config.js
@@ -0,0 +1,7 @@
+/*
+Configurations
+Credits: original code by Dacal, Matchstic & Junesiphone
+*/
+
+var Clock = "24h";  // choose between "12h" or "24h"
+var Lang = "en";   // choose between "en", "ca", "fr", "de", "it", or "cz"
