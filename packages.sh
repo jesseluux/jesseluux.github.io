7d183b8aed4a3d0dc9849a87ccafe83c2e6e3816
jesseluux <jesse96pesola@gmail.com>
2016-08-31 22:57:28 +0000

    added JP_LS_2

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/JP_LS_1 debs
 dpkg-deb -bZgzip projects/JP_LS_1_dark debs
+dpkg-deb -bZgzip projects/JP_LS_2 debs
 
 #dpkg-deb -bZgzip projects/<project name> <output folder>
---
+++ projects/JP_LS_2/var/mobile/Library/LockHTML/JP_LS_2/Config.js
@@ -0,0 +1,2 @@
+var time = "24h";  // choose between "12h" or "24h"
+var blur = true; // choose between true and false, this changes the text shadow to either be blurred or not
